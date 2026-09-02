9e9fdc7a4824cc07f6571128a3d6bfba5cd33fc6
chakronwork <chakronwork@users.noreply.github.com>
2026-09-02 11:06:06 +0700

    feat: add task sleep block wakeup v0.0.19

--- include/gdt.h
+++ include/gdt.h
@@ -1,6 +1,26 @@
 #ifndef GDT_H
 #define GDT_H
 
+/*
+ * ============================================================
+ * GDT selectors
+ * ============================================================
+ *
+ * GDT layout:
+ *
+ *   0x00  null
+ *   0x08  kernel code  (DPL 0)
+ *   0x10  kernel data  (DPL 0)
+ *   0x18  user code    (DPL 3)
+ *   0x20  user data    (DPL 3)
+ */
+#define GDT_KERNEL_CODE 0x08U
+#define GDT_KERNEL_DATA 0x10U
+
+#define GDT_USER_CODE   0x18U
+#define GDT_USER_DATA   0x20U
+
+
 void gdt_init(void);
 
 #endif
